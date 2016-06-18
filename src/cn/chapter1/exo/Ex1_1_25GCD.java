5387ea3d06a0d93ccc306110adcc7d187b8f03b3
雨天 <77202714@qq.com>
2016-06-18 12:03:30 +0800

    done for 1.25 & 1.27

---
+++ src/cn/chapter1/exo/Ex1_1_25GCD.java
@@ -0,0 +1,27 @@
+package cn.chapter1.exo;
+
+import edu.princeton.cs.algs4.*;
+
+public class Ex1_1_25GCD {
+
+	public static void main(String[] args) {
+
+		StdOut.print("Input 2 nums: ");
+
+		int num1 = StdIn.readInt();
+		int num2 = StdIn.readInt();
+
+		StdOut.print("GCD: " + gcd(num1, num2));
+	}
+
+	static int gcd(int a, int b) {
+		int max = Math.max(a, b);
+		int min = Math.min(a, b);
+		if (min == 0)
+			return max;
+		else {
+			return gcd(min, max % min);
+		}
+	}
+
+}
---
+++ src/cn/chapter1/exo/Ex1_1_27Binomial.java
@@ -0,0 +1,52 @@
+package cn.chapter1.exo;
+
+import edu.princeton.cs.algs4.StdArrayIO;
+import edu.princeton.cs.algs4.StdOut;
+
+public class Ex1_1_27Binomial {
+
+	public static void main(String[] args) {
+
+		StdOut.println("Recusif: " + binomial(n, k, p));
+		StdArrayIO.print(x);
+		StdOut.println("Iteration: " + binomialArray(n, k, p));
+	}
+
+	static int n = 2;
+	static int k = 2;
+	static double p = 0.1;
+	static double[][] x = new double[n + 1][k + 1];
+
+	public static double binomialArray(int n, int k, double p) {
+
+		double[][] a = new double[n + 1][k + 1];
+		// for the (0, i) row, in case of 0 - 1, it terminate its value is 0.
+		// but for the (i,0) col, i - 2 where i >= 1,
+		// it will get the value from (1.0 - p) * binomial(n - 1, k, p)
+		a[0][0] = 1.0;
+		for (int i = 1; i < n + 1; i++) {
+			a[i][0] = (1.0 - p) * a[i - 1][0];
+		}
+
+		for (int i = 1; i < n + 1; i++) {
+			for (int j = 1; j < k + 1; j++) {
+				a[i][j] = (1.0 - p) * a[i - 1][j] + p * a[i - 1][j - 1];
+			}
+		}
+
+		StdArrayIO.print(a);
+
+		return a[n][k];
+	}
+
+	public static double binomial(int n, int k, double p) {
+		if (n == 0 && k == 0) {
+			x[n][k] = 1.0;
+			return 1.0;
+		}
+		if (n < 0 || k < 0)
+			return 0.0;
+		x[n][k] = (1.0 - p) * binomial(n - 1, k, p) + p * binomial(n - 1, k - 1, p);
+		return (1.0 - p) * binomial(n - 1, k, p) + p * binomial(n - 1, k - 1, p);
+	}
+}
